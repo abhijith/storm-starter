8c31a5ebaeecbead3612aed978274cfef91b3b8c
Abhijith <abhijithg@gmail.com>
2016-01-12 11:06:41 +0530

    * Upgrade to clojure 1.7 and STORM 0.10.0 * cider nrepl added as dev dependency for emacs interop

--- project.clj
+++ project.clj
@@ -4,22 +4,18 @@
   :test-paths ["test/jvm"]
   :resource-paths ["multilang"]
   :aot :all
-  :repositories {
-;;                 "twitter4j" "http://twitter4j.org/maven2"
-                 }
+  :repositories {}
 
-  :dependencies [
-;;                 [org.twitter4j/twitter4j-core "2.2.6-SNAPSHOT"]
-;;                 [org.twitter4j/twitter4j-stream "2.2.6-SNAPSHOT"]
-                   [commons-collections/commons-collections "3.2.1"]
-                 ]
-
+  :dependencies [[commons-collections/commons-collections "3.2.1"]]
   :profiles {:dev
-              {:dependencies [[storm "0.9.0.1"]
-                              [org.clojure/clojure "1.4.0"]
-                              [org.testng/testng "6.8.5"]
-                              [org.easytesting/fest-assert-core "2.0M8"]
-                              [org.mockito/mockito-all "1.9.0"]
-                              [org.jmock/jmock "2.6.0"]]}}
+             {:dependencies [[org.apache.storm/storm-core "0.10.0"]
+                             [cider/cider-nrepl "0.11.0-SNAPSHOT"]
+                             [org.clojure/tools.nrepl "0.2.12"]
+                             [org.clojure/clojure "1.7.0"]
+                             [org.testng/testng "6.8.5"]
+                             [org.easytesting/fest-assert-core "2.0M8"]
+                             [org.mockito/mockito-all "1.9.0"]
+                             [org.jmock/jmock "2.6.0"]]}}
   :min-lein-version "2.0.0"
+  :main storm.starter.clj.word-count
   )
